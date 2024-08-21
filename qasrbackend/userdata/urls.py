9b5cc0866eb66eb41cdfa8edcddda47d57f6d4b6
Abdulrahman-MA <Abdulrahman_m_a@outlook.com>
2024-08-22 00:21:28 +0300

    we are fucked !!!

--- qasrbackend/userdata/urls.py
+++ qasrbackend/userdata/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import reset_password, signup, login
+from .views import reset_password, signup, login, change_email
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', login, name='login'),
     path('passwordreset/', reset_password, name='password_reset'),
+    path('changeemail', change_email, name='change_email')
 ]
--- qasrbackend/userdata/views.py
+++ qasrbackend/userdata/views.py
@@ -1,10 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, get_user_model
-from .serializer import UserSerializer, ResetPasswordEmailRequestSerializer
+from .serializer import UserSerializer, ResetPasswordEmailRequestSerializer, EmailChangeSerializer
 from .models import ResetPasswordToken
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib import messages
 
 @api_view(['GET'])
 def login(self, request, *args, **kwargs):
@@ -53,3 +56,15 @@ def reset_password(request):
         return Response({"success": "Password reset email sent"}, status=status.HTTP_200_OK)
     else:
         return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+@login_required
+@api_view(['PATCH'])
+def change_email(request):
+    serializer = EmailChangeSerializer(data=request.data)
+    if serializer.is_valid():
+        new_email = serializer.validated_data['new_email']
+        request.user.email = new_email
+        request.user.save()
+        return Response({'detail': 'Email updated successfully'}, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
