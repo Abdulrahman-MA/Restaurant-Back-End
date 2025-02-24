faddfa2e8568357f665151c83d6c2ab791643480
Abdulrahman-MA <Abdulrahman_m_a@outlook.com>
2025-02-24 18:18:00 +0200

    hi

--- qasrbackend/qasrbackend/settings.py
+++ qasrbackend/qasrbackend/settings.py
@@ -214,5 +214,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = 'atawfek150@gmail.com'
-EMAIL_HOST_PASSWORD = 'xgyu yhtr mjuz sfdy'
+EMAIL_HOST_USER = env
+EMAIL_HOST_PASSWORD = env
